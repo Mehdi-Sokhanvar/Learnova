8fcf7ed312fedf23dd8cade0ccafd23c5fcaf3c0
Mehdi <mohammadmahdi.sokhanvar@gmail.com>
2025-04-05 08:17:41 +0330

    add some utility

---
+++ src/main/java/org/learnova/lms/util/Messages.java
@@ -0,0 +1,30 @@
+package org.learnova.lms.util;
+
+import java.util.Locale;
+
+public class Messages {
+    public static final String EMAIL_EXIST = "This email already exists";
+
+    public static final String USER_NOT_FOUND = "This user does not exist";
+
+    public static final String USER_ALREADY_EXISTS = "This user already exists";
+    public static final String USER_UPDATE_SUCCESS = "Successfully updated user successfully : ID => %s";
+    public static final String ROLE_NOT_FOUND = "This role does not exist";
+    public static final String USER_NOT_FOUND_BY_THIS_ID = "User Not Found in DB: %s";
+    public static final String START_DATE_CANNOT_BE_BEFORE_END_DATE = "Start Date cannot be before End Date";
+
+    public static final String USER_ASSIGN_COURSE_SUCCESS = "Successfully assigned course successfully : ID => %s";
+    public static final String COURSE_NOT_FOUND_BY_THIS_ID = "Course Not Found in DB: %s";
+    public static final String TEACHER_NOT_ASSIGNED_IN_THIS_COURSE = "Teacher not assigned in course : ID => %s ";
+    public static final String EXAM_NOT_FOUND_BY_THIS_ID = "\"Exam Not Found in DB: %s";
+    public static final String ADD_EXAM_SUCCESSFULLY = "successfully added exam successfully ";
+    public static final String EXAM_UPDATE_SUCCESFULLY = "Successfully updated exam successfully : ID => %s";
+
+    public static final String DELETE_EXAM_SUCCESFULLY="Successfully deleted exam successfully : ID => %s";
+    public static final String USER_NOT_ENROLLED_IN_COURSE = "User Not Enrolled In Course : ID => %s ";
+    public static final String COURSE_UPDATE_SUCCESFULY = "Course Successfully updated : ID => %s ";
+    public static final String DELETE_COURSE_SUCCESFULLY = "Course Succesfully deleted : ID => %s";
+    public static final String SUCCESFULLY_DELETE_STUDENT_FROM_COURSE = "Succesfully Delete Student %s From Course %s";
+
+    public static final String DATE_EXAM_BETWEEN_START_DATE_AND_END_DATE="Date exam Should Start berween Start data and exam Date Course";
+}
