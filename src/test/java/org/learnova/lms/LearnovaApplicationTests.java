5a98cc74ea182b2aa5f8468562b27565f7b257ff
Mehdi <mohammadmahdi.sokhanvar@gmail.com>
2025-04-05 08:05:14 +0330

    initial liquibase for data migration

---
+++ src/test/java/org/learnova/lms/LearnovaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.learnova.lms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnovaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
