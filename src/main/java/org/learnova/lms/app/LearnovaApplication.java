c15db2359897596274df9126e1af4c6416155a26
Mehdi <mohammadmahdi.sokhanvar@gmail.com>
2025-04-05 08:18:53 +0330

    feat(app): initialize Spring Boot application skeleton
    - Add base `LearnovaApplication` class with `@SpringBootApplication`
    - Configure entry point for Spring Boot runtime

---
+++ src/main/java/org/learnova/lms/app/LearnovaApplication.java
@@ -0,0 +1,13 @@
+package org.learnova.lms.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnovaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnovaApplication.class, args);
+    }
+
+}
